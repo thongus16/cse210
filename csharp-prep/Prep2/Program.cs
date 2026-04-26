990ebaba212e8adee45fc13c5c63275c98d3c0e0
thongus16 <bombh31@gmail.com>
2026-04-25 21:11:59 -0600

    W01

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter;
+        if (grade >= 90)
+        {
+            letter = "an A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "a B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "a C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "a D";
+        }
+        else
+        {
+            letter = "an F";
+        }
+        string sign = "";
+        if (grade >= 60 && grade < 90 && grade % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (grade >= 60 && grade % 10 < 3)
+        {
+            sign = "-";
+        }
+        Console.WriteLine($"You got {letter}{sign}.");
+        if (grade > 70)
+        {
+            Console.WriteLine("Congratulations, you passed.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed.");
+        }
     }
 }
